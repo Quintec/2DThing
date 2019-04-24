44185080541bea19397b8e64af7fb7194e61b130
Quintec <huang.lucas16@gmail.com>
2019-04-24 10:50:19 -0400

    start map

--- src/SpriteLoc.java
+++ src/SpriteLoc.java
@@ -1,6 +1,9 @@
 public enum SpriteLoc {
     BOY(0, 8), GIRL(3, 8), SKELETON(6, 8),
-    OOZE(0, 12), BAT(3, 12), GHOST(6, 12), SPIDER(9, 12);
+    OOZE(0, 12), BAT(3, 12), GHOST(6, 12), SPIDER(9, 12),
+    
+    FLOOR(8, 6), 
+    WALL_VERTICAL(0, 8), WALL(0, 9), WALL_CORNER_BOTTOM(0, 10), WALL_CORNER_TOP(0, 11);
     
     private int x;
     private int y;
