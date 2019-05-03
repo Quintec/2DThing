892fa011d686c1b42a94f3b9ed48a0597d794291
CrystalEye42 <49692820+CrystalEye42@users.noreply.github.com>
2019-05-03 15:19:04 -0400

    Add files via upload

--- src/BasicEnemy.java
+++ src/BasicEnemy.java
@@ -1,23 +1,36 @@
 public class BasicEnemy extends Enemy {
-  private final double SPEED = 1;
+  private final double SPEED = 0.5;
+  private final Location loc;
   
     public BasicEnemy(int xc, int yc, String path, Character main) {
         super(xc, yc, path, main);
+        loc = new Location(xc,yc);  
     }
     
     public BasicEnemy(int xc, int yc, SpriteLoc sl, Character main) {
         super(xc, yc, sl, main);
+        loc = new Location(xc,yc);  
     }
 
     @Override
     public void update() {
         int dx = this.target.x-this.x;
         int dy = this.target.y-this.y;
-        //this.incX((int)(SPEED*dx/Math.sqrt(dx*dx+dy*dy)));
-        //this.incY((int)(SPEED*dy/Math.sqrt(dx*dx+dy*dy)));
-        if (Math.random()<0.5)
+        int xOld = this.x;
+        int yOld = this.y;
+        loc.translate(SPEED*dx/Math.sqrt(dx*dx+dy*dy),SPEED*dy/Math.sqrt(dx*dx+dy*dy));
+        if (!this.setX((int)loc.getX()))
+          loc.setX(xOld);
+        if (!this.setY((int)loc.getY()))
+          loc.setY(yOld);
+        //if (xOld==this.x)
+          //loc.setX(xOld);
+        //if (yOld==this.y)
+          //loc.setY(yOld);
+        
+        /*if (Math.random()<0.5)
           this.incX((int)(SPEED*dx/Math.abs(dx)));
         else
-          this.incY((int)(SPEED*dy/Math.abs(dy)));
+          this.incY((int)(SPEED*dy/Math.abs(dy)));*/
     }
 }
